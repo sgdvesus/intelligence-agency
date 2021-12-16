93683e37b2bfa30cd127e59c77b55d82848c453a
sgdvesus <jesusvlzez@gmail.com>
2021-12-16 05:26:56 -0600

    feat: Add new parse method to Spider. Get link, title and description of declassified documents.

--- intelligence_agency/intelligence_agency/spiders/intelly.py
+++ intelligence_agency/intelligence_agency/spiders/intelly.py
@@ -1,26 +1,37 @@
 import scrapy
-
-class Spiderintelly(scrapy.Spider):
+import os
+os.remove('intelly.json')
+class SpiderIntelly(scrapy.Spider):
     name = 'intelly'
-    start_urls =[
+    start_urls = [
         'https://www.cia.gov/readingroom/historical-collections'
     ]
-    custom_setting = {
+    custom_settings = {
         'FEED_URI': 'intelly.json',
         'FEED_FORMAT': 'json',
         'FEED_EXPORT_ENCODING': 'utf-8',
-        'CONCURRENT_REQUEST': 24,
-        'MEMUSAGE__LIMIT_MB': 2048,
-        'MEMUSAGE_NOTIFY_MAIL': ['admin@mail.com'],
+        #'CONCURRENT_REQUEST': 24,
+        #'MEMUSAGE__LIMIT_MB': 2048,
+        #'MEMUSAGE_NOTIFY_MAIL': ['admin@mail.com'],
         'ROBOTSTXT_OBEY': True,
-        'USERAGENT': 'intelly'
+        'USER_AGENT': 'intelly'
     }
     def parse(self, response):
         links_desclasified = response.xpath('//a[starts-with(@href,"collection")]/@href').getall()
 
         for link in links_desclasified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
 
 
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]//p[not(@class) and not(strong)]/text()').get()
+        #response.xpath('//div[@class="field-item even"]/p[3]/text()').get()
+        #paragraph = response.xpath('//div[@class="field-item even"]/p[not(@child)]/text()').get()
+
+        yield {
+            'link': link,
+            'title': title,
+            'paragraph': paragraph
+        }
